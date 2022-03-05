c44a0869088854c9f8c6913438a2bc411b07d319
kwonja <kownja87@gmail.com>
2022-03-05 16:26:26 +0900

    [BOJ]2xn타일링2

---
+++ kwonja/2×n 타일링2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int a[1000001] = { 0, };
+int main(void)
+{
+	int n;
+	cin >> n;
+	a[1] = 1;
+	a[2] = 3;
+	for (int i = 3; i <= n; i++)
+	{
+		// N=N-1 + N-2
+		a[i] = ((a[i - 1] + a[i - 2] * 2) % 10007);
+	}
+	cout << a[n];
+	return 0;
+}
