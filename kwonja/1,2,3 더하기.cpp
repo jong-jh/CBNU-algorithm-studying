b5d17d3905857d2ae3a17820b94399334536b6d6
kwonja <kownja87@gmail.com>
2022-03-06 15:17:58 +0900

    [BOJ] 1,2,3,더하기

---
+++ kwonja/1,2,3 더하기.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int a[1000001] = { 0,1,2,4, };
+int main(void)
+{
+	int n, k;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> k;
+		for (int j = 4; j <= k; j++)
+		{
+			if (a[j] == 0)a[j] = a[j - 1] + a[j - 2] + a[j - 3];
+		}
+		cout << a[k] << endl;
+	}
+	return 0;
+}
