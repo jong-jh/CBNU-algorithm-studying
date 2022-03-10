9bd39b198da7d88293ec2f6ec05aa7b46debeb1b
Cat-Hanbit <jmjung1997@naver.com>
2022-03-10 21:52:49 +0900

    [BOJ] test

---
+++ jaemin/test.py
@@ -0,0 +1,6 @@
+import pickle
+profile_file=open("profile.pickle","wb")
+profile={"이름": "박명수", "나이": 30, "취미":["축구,"골프","코딩"]}
+print(profile)
+pickle.dump(profile,profile_file)
+profile_file.close()
