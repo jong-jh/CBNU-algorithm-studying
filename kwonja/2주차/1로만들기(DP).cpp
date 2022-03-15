595073fb8a38a370fe0e259a1f5c0f857256d25d
kwonja <kownja87@gmail.com>
2022-03-15 22:11:15 +0900

    [BOJ]1로만들기

---
+++ kwonja/2주차/1로만들기(DP).cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int a[1000001] = { 0, };
+int main(void)
+{
+	int n;
+	cin >> n;
+	for (int i = 2; i <= n; i++)
+	{
+		a[i] = a[i - 1] + 1;
+		if (i % 2 == 0)
+			a[i] = min(a[i], a[i / 2] + 1);
+		if (i % 3 == 0)
+			a[i] = min(a[i], a[i / 3] + 1);
+	}
+	cout << a[n];
+	return 0;
+}
