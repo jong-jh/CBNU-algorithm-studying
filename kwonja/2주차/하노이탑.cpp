b45b0db6e3a25ea6cd6d4017d5eed64ccc023e23
kwonja <kownja87@gmail.com>
2022-03-15 22:11:48 +0900

    [BOJ]하노이탑

---
+++ kwonja/2주차/하노이탑.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+void func1(int num, int start, int mid, int end)
+{
+	if (num == 1)
+	{
+		printf("%d %d\n", start, end);
+		return;
+	}
+	func1(num - 1, start, end, mid);
+	printf("%d %d\n", start, end);
+	func1(num - 1, mid, start, end);
+}
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+	printf("%d\n", (1 << n) - 1);
+	func1(n, 1, 2, 3);
+	return 0;
+}
